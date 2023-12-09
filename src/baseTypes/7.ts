3edd62117770ee7d439a1b03b59f7494b4e58d8c
KulibabaYuliia <kulibaba120@gmail.com>
2023-12-09 17:11:03 +0200

    fisish base types

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,17 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Weekend {
+  Sunday,
+  Monday,
+  Thuesday,
+  Wensday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+const isWeekend = (day: Weekend): boolean => {
+  return day === Weekend.Saturday || day === Weekend.Sunday;
+};
