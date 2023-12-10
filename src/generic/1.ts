b0471fcf9984d1aac67de9ba85c3530743588cb8
KulibabaYuliia <kulibaba120@gmail.com>
2023-12-10 19:28:22 +0200

    add promise

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -4,7 +4,7 @@
 */
 
 function getPromise(): Promise<Array<string | number>> {
-  return new Promise((resolve) => {
+  return new Promise<Array<string | number>>((resolve) => {
     resolve(["Text", 50]);
   });
 }
